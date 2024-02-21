042dfb39139c461567c16ed62538ab8fca036c86
Krste Rajchevski <rajchevski.krste@live.com>
2024-02-21 12:51:10 +0100

    Fixed Safari issue for mobile by setting min-height

--- components/phone-frame.tsx
+++ components/phone-frame.tsx
@@ -2,12 +2,10 @@ import { ReactNode } from "react";
 
 export default function PhoneFrame({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full md:max-w-[414px] md:h-[736px] md:max-h-[736px] overflow-hidden md:border-8 md:border-black md:rounded-3xl md:ring-offset-white">
+    <div className="w-full min-h-screen md:max-w-[414px] md:h-[736px] md:max-h-[736px] overflow-hidden md:border-8 md:border-black md:rounded-3xl md:ring-offset-white">
       <div className="ring-slate-600 min-h-full w-full pb-12 relative">
         {children}
       </div>
     </div>
   );
 }
-
-// pt-16
